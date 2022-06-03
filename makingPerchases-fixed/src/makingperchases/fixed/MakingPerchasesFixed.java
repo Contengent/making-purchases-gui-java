3951e3ff2314cae75dbad3e8333116c84fc02448
Contengent <103345406+Contengent@users.noreply.github.com>
2022-06-03 00:26:11 -0400

    Add files via upload

---
+++ makingPerchases-fixed/src/makingperchases/fixed/MakingPerchasesFixed.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package makingperchases.fixed;
+
+/**
+ * @author 
+ *
+ *   /\_/\  _     ___
+ *  |>'v'<||"| -/\, ,`>
+ *     -=Edelstein=-
+ * 
+ */
+public class MakingPerchasesFixed {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
